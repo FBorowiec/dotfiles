4635f829d27281feb39e3752259f5b8a29530ce3
Franek Borowiec <franek.borowiec@gmail.com>
2021-05-07 19:47:01 +0200

    Env install scripts

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,3 +5,8 @@ echo "Installing Terminator config file.."
 mkdir -p ~/.config/terminator/
 cp terminator/config ~/.config/terminator/config
 
+# Installing NVim
+
+# ln -s ~/.vim ~/.config/nvim
+# ln -s ~/.vimrc ~/.config/nvim/init.vim
+
