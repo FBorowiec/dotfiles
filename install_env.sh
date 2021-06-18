a231b0235ba289ca62bb91ad69722959b33d7d33
Franek Borowiec <franek.borowiec@gmail.com>
2021-06-18 19:11:41 +0200

    Install deps script update

--- install_env.sh
+++ install_env.sh
@@ -6,22 +6,23 @@
 CHECK_MARK="\033[0;32m\u2713"
 ERROR_MARK="\033[0;31m\u2717"
 
-echo -e "Upgrading the system..."
-sudo apt -y -qq update
-sudo apt -y -qq upgrade
-echo -e "$CHECK_MARK \e[1mUpgrading system succeeded!\e[0m\e[32m\e[39m"
-
 echo -e "Installing dependencies..."
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get -y -qq update
 sudo apt install -y software-properties-common apt-transport-https wget git curl wget \
     simplescreenrecorder nload gnome-tweaks chrome-gnome-shell gnome-shell-extensions \
     python3-neovim pynvim python3-venv zsh fzf ripgrep terminator tmux ccls ctags nodejs \
-    npm global clangd lua5.1 lua-nvim ninja-build gettext libtool libtool-bin autoconf \
-    automake cmake g++ pkg-config unzip # neovim
-sudo npm install -g yarn && yarn --registry "https://registry.npmjs.org"
+    yarn global clangd lua5.1 lua-nvim ninja-build gettext libtool libtool-bin autoconf \
+    automake cmake g++ gcc pkg-config unzip build-essential # neovim
 pip3 install -U jedi-language-server
 echo -e "$CHECK_MARK \e[1mInstalling dependecies succeeded!\e[0m\e[32m\e[39m"
 
 cd && git clone https://github.com/neovim/neovim.git && cd neovim && make && sudo make install
+
+sudo apt -y -qq upgrade
+echo -e "$CHECK_MARK \e[1mUpgrading system succeeded!\e[0m\e[32m\e[39m"
 
 # Exit on success
 echo -e "$CHECK_MARK \e[1mScript execution succeeded!\e[0m\e[32m\e[39m"
