b8962c97c1006134eb7e68d30da9f04c4e516763
Franek Borowiec <franek.borowiec@gmail.com>
2021-06-26 18:06:51 +0200

    Current config

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+echo -e "Configuring dotfiles..."
 # Installing terminator config
-echo "Configuring terminator..."
 mkdir -p ~/.config/terminator/
 cp terminator/config ~/.config/terminator/config
 
 # Installing NVim
-echo -e "Configuring nvim..."
 cp -r nvim ~/.config/nvim
 
 # Configuring Powerlevel10k and Zsh
-echo -e "Configuring zsh..."
 cp zsh/p10k.zsh ~/.p10k.zsh
 cp zsh/zshrc ~/.zshrc
 cp zsh/inputrc ~/.inputrc
 
-echo -e "Configuring git..."
+# git config
 cp git/gitconfig ~/.gitconfig
+
+# Tmux config
+cp tmux/tmux.conf ~/.tmux.conf
