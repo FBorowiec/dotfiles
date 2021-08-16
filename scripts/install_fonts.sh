d8a6d9852e42f91edeee7c4ba83a53c5f746e71a
Franek Borowiec <franek.borowiec@gmail.com>
2021-08-16 23:06:03 +0200

    Add fonts to install scripts

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -5,6 +5,7 @@ cd ~/.local/share/fonts
 echo -e "Installing nerd fonts..."
 curl -sS https://webinstall.dev/nerdfont | bash
 curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 curl -flo "MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 curl -flo "MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 curl -flo "MesloLGS NF Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
