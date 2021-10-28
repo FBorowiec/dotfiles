151221a29dce1a3c40c00094c22fc6cb9bce2064
Franek Borowiec <franek.borowiec@gmail.com>
2021-10-28 14:52:39 +0200

    Ftplugin

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -7,3 +7,6 @@ vim.g.mapleader = ' '  -- 'vim.g' sets global variables
 options = { noremap = true }
 
 map('n', '<C-p>', ':NvimTreeToggle<cr>:NvimTreeRefresh<cr>', options)
+
+-- Sensible defaults
+-- map('', 'Q', '') -- disable
