8add6e9754cdee09dad18aba19d9805e6d05ad21
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-23 16:22:34 +0200

    Init lua

---
+++ nvim-adp/.config/nvim/init.lua
@@ -0,0 +1,12 @@
+-- General config
+local fn = vim.fn
+if not packer_plugins then
+  vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = " "
+  vim.opt.termguicolors = true
+end
+
+require('settings')
+require('plugins')
+require('mappings')
