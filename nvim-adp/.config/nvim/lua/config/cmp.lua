bc4cf2da9c991a81e534355999ef14784ced8614
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-23 16:22:46 +0200

    Nvim config

---
+++ nvim-adp/.config/nvim/lua/config/cmp.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+function M.setup()
+
+    local cmp = require 'cmp'
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+     },
+     formatting = {
+       format = require'lspkind'.cmp_format({})
+     },
+      mapping = {
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+      },
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+        { name = 'path' },
+        { name = 'buffer' },
+      },
+    }
+
+    vim.cmd([[ autocmd FileType bzl lua require'cmp'.setup.buffer { sources = { { name = 'bazel' }, { name = 'buffer' } } } ]])
+end
+
+return M
+
---
+++ nvim-adp/.config/nvim/lua/config/gitsigns.lua
@@ -0,0 +1,16 @@
+local M = {}
+function M.setup()
+    require('gitsigns').setup{
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+      end
+    }
+end
+
+return M
