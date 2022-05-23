c88a589a883f93c4101b1c464232492add5602d8
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-23 16:25:24 +0200

    Update old nvim settings

--- nvim/.config/nvim/lua/config/harpoon.lua
+++ nvim/.config/nvim/lua/config/harpoon.lua
@@ -5,5 +5,5 @@ require("harpoon").setup({
     }
 })
 
-vim.api.nvim_set_keymap('n', '<leader>h', ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-h>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>h', ":lua require('harpoon.mark').add_file()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-h>', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true })
