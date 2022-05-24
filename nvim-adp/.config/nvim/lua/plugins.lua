4a2f103722eba33563648c252baeb27febd9e970
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-24 19:37:43 +0200

    Update TODOs

--- nvim-adp/.config/nvim/lua/plugins.lua
+++ nvim-adp/.config/nvim/lua/plugins.lua
@@ -7,9 +7,7 @@ local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' })
 
 -- Check "awesome-neovim plugins" on: https://github.com/rockerBOO/awesome-neovim/blob/main/README.md
-return require('packer').startup(function()
-    local use = require('packer').use
-
+return require('packer').startup(function(use)
     -- Packer can manage itself
     use { 'wbthomason/packer.nvim' }
 
