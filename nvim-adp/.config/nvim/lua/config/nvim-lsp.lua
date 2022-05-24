aafb0c04959b3a043525cc648e92af3c8112ce03
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-24 16:52:46 +0200

    Lsp setup for lua

--- nvim-adp/.config/nvim/lua/config/nvim-lsp.lua
+++ nvim-adp/.config/nvim/lua/config/nvim-lsp.lua
@@ -1,6 +1,35 @@
 local M = {}
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/?.lua")
+table.insert(runtime_path, "lua/?/?/?.lua")
+
+local lua_settings = {
+  Lua = {
+    runtime = {
+      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+      version = 'LuaJIT',
+      -- Setup your lua path
+      path = runtime_path,
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'},
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = vim.api.nvim_get_runtime_file("", true),
+    },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {
+      enable = false,
+    },
+  },
+}
+
 function M.setup()
-    local servers = { "clangd", "pylsp", "bashls" }
+    local servers = { "clangd", "pylsp", "sumneko_lua", "bashls" }
     require("nvim-lsp-installer").setup({ automatic_installation = true})
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -11,6 +40,9 @@ function M.setup()
         local config = {
             capabilities = capabilities,
         }
+        if server == "sumneko_lua" then
+          config.settings = lua_settings
+        end
         if server == "pylsp" then
             config.settings = { python = { analysis = { extraPaths = { vim.fn.getcwd() } } } }
         end
