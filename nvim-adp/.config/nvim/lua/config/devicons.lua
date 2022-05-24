bf8c119cb1fa588ec240aa92624e36bd6d9df2a9
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-24 14:52:14 +0200

    Install missing nerdfont for devicons

--- nvim-adp/.config/nvim/lua/config/devicons.lua
+++ nvim-adp/.config/nvim/lua/config/devicons.lua
@@ -1,6 +1,17 @@
 local M = {}
 
 function M.setup()
+    vim.cmd([[
+        if empty(glob("~/.local/share/fonts/ubuntu-mono-powerline-ttf"))
+            echo "Installing powerline fonts..\n"
+            silent !mkdir -p ~/.fonts/ubuntu-mono-powerline-ttf
+            execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Bold Italic.ttf"'
+            execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Bold.ttf"'
+            execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Italic.ttf"'
+            execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline.ttf"'
+            execute 'silent !fc-cache -vf'
+        endif
+    ]])
     require'nvim-web-devicons'.setup {
      -- your personnal icons can go here (to override)
      -- DevIcon will be appended to `name`
@@ -8,157 +19,188 @@ function M.setup()
       zsh = {
         icon = "",
         color = "#81A1C1",
-        name = "zsh"
+        name = "zsh",
+        cterm_color = "39"
       },
       ['zshrc'] = {
         icon = "",
         color = "#81A1C1",
-        name = "zshrc"
+        name = "zshrc",
+        cterm_color = "39"
       },
       ['inputrc'] = {
         icon = "",
         color = "#81A1C1",
-        name = "inputrc"
+        name = "inputrc",
+        cterm_color = "39"
       },
       ['.gitattributes'] = {
         icon = '',
         color = '#BF616A',
-        name = 'gitattributes'
+        name = 'gitattributes',
+        cterm_color = "203"
       },
       ['.gitconfig'] = {
         icon = '',
         color = '#BF616A',
-        name = 'gitconfig'
+        name = 'gitconfig',
+        cterm_color = "203"
       },
       gitconfig = {
         icon = '',
         color = '#BF616A',
-        name = 'gitconfig'
+        name = 'gitconfig',
+        cterm_color = "203"
       },
       ['.gitignore'] = {
         icon = '',
         color = '#BF616A',
-        name = 'gitignore'
+        name = 'gitignore',
+        cterm_color = "203"
       },
       gitignore = {
         icon = '',
         color = '#BF616A',
-        name = 'gitignore'
+        name = 'gitignore',
+        cterm_color = "203"
       },
       ['.lfsconfig'] = {
         icon = '',
         color = '#BF616A',
-        name = 'lfsconfig'
+        name = 'lfsconfig',
+        cterm_color = "203"
       },
       ['.gitlint'] = {
         icon = '',
         color = '#BF616A',
-        name = 'gitlint'
+        name = 'gitlint',
+        cterm_color = "203"
       },
       ['TODO'] = {
         icon = '',
         color = '#B48EAD',
-        name = 'TODO'
+        name = 'TODO',
+        cterm_color = "183"
       },
       ['BUILD'] = {
         icon = '',
         color = '#A3BE8C',
-        name = 'BUILD'
+        name = 'BUILD',
+        cterm_color = "2"
       },
       ['WORKSPACE'] = {
         icon = '',
         color = '#A3BE8C',
-        name = 'WORKSPACE'
+        name = 'WORKSPACE',
+        cterm_color = "2"
       },
       ['.bazeliskrc'] = {
         icon = '',
         color = '#A3BE8C',
-        name = 'bazeliskrc'
+        name = 'bazeliskrc',
+        cterm_color = "2"
       },
       ['.bazelrc'] = {
         icon = '',
         color = '#A3BE8C',
-        name = 'bazelrc'
+        name = 'bazelrc',
+        cterm_color = "2"
       },
       bazelrc = {
         icon = '',
         color = '#A3BE8C',
-        name = 'bazelrc'
+        name = 'bazelrc',
+        cterm_color = "2"
       },
       bzl = {
         icon = '',
         color = '#A3BE8C',
-        name = 'bzl'
+        name = 'bzl',
+        cterm_color = "2"
       },
       ['.bazelversion'] = {
         icon = '',
         color = '#A3BE8C',
-        name = 'bazelversion'
+        name = 'bazelversion',
+        cterm_color = "2"
       },
       txt = {
         icon = '',
         color = '#88C0D0',
-        name = 'txt'
+        name = 'txt',
+        cterm_color = "14"
       },
       ['version'] = {
         icon = '',
         color = '#B48EAD',
-        name = 'version'
+        name = 'version',
+        cterm_color = "183"
       },
       config = {
         icon = '',
         color = '#6D8086',
-        name = 'config'
+        name = 'config',
+        cterm_color = "8"
       },
       ['.coveragerc'] = {
         icon = '',
         color = '#6D8086',
-        name = 'coveragerc'
+        name = 'coveragerc',
+        cterm_color = "8"
       },
       ['.pylintrc'] = {
         icon = '',
         color = '#6D8086',
-        name = 'pylintrc'
+        name = 'pylintrc',
+        cterm_color = "8"
       },
       ['Jenkinsfile'] = {
         icon = '',
         color = '#ECEFF4',
-        name = 'Jenkinsfile'
+        name = 'Jenkinsfile',
+        cterm_color = "15"
       },
       fcdl = {
         icon = 'ﲩ',
         color = '#ECEFF4',
-        name = 'fcdl'
+        name = 'fcdl',
+        cterm_color = "15"
       },
       ['.clang-format'] = {
         icon = '',
         color = '#ECEFF4',
-        name = 'ClangFormat'
+        name = 'ClangFormat',
+        cterm_color = "15"
       },
       ['.clang-tidy'] = {
         icon = '',
         color = '#ECEFF4',
-        name = 'ClangTidy'
+        name = 'ClangTidy',
+        cterm_color = "15"
       },
       ['.clang-tidy-minimal'] = {
         icon = '',
         color = '#ECEFF4',
-        name = 'ClangTidyMinimal'
+        name = 'ClangTidyMinimal',
+        cterm_color = "15"
       },
       msg = {
         icon = '',
         color = '#ECEFF4',
-        name = 'msg'
+        name = 'msg',
+        cterm_color = "15"
       },
       csv = {
         icon = '',
         color = '#A3BE8C',
-        name = 'csv'
+        name = 'csv',
+        cterm_color = "155"
       },
       xlsx = {
         icon = '',
         color = '#A3BE8C',
-        name = 'xlsx'
+        name = 'xlsx',
+        cterm_color = "155"
       }
      };
      -- globally enable default icons (default to false)
@@ -168,16 +210,4 @@ function M.setup()
 end
 
 return M
-
-vim.cmd([[
-    if empty(glob("~/.local/share/fonts/ubuntu-mono-powerline-ttf"))
-        echo "Installing powerline fonts..\n"
-        silent !mkdir -p ~/.fonts/ubuntu-mono-powerline-ttf
-        execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Bold Italic.ttf"'
-        execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Bold.ttf"'
-        execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline Italic.ttf"'
-        execute 'silent !wget -q -P ~/.local/share/fonts/ubuntu-mono-powerline-ttf/ "https://raw.githubusercontent.com/powerline/fonts/master/UbuntuMono/Ubuntu Mono derivative Powerline.ttf"'
-        execute 'silent !fc-cache -vf'
-    endif
-]])
 
