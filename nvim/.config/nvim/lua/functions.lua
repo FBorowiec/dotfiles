c36cbb1269a768868f5ec1b9689689275ac77b96
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-25 12:31:54 +0200

    Remove deprecated functions

--- nvim/.config/nvim/lua/functions.lua
+++ nvim/.config/nvim/lua/functions.lua
@@ -19,17 +19,3 @@ vim.cmd([[
         exec "find " . filepath . filename . filetype
     endfun
 ]])
-
--- trim whitespaces
-vim.cmd([[
-    fun! TrimWhitespace()
-        let l:save = winsaveview()
-        keeppatterns %s/\s\+$//e
-        call winrestview(l:save)
-    endfun
-
-    augroup TRIM_ON_SAVE
-        autocmd!
-        autocmd BufWritePre * :call TrimWhitespace()
-    augroup END
-]])
