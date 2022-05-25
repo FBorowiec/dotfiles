8e6bde1a1334bdf558d1f8e8dd93c96ab5ada0e9
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-25 15:16:14 +0200

    Dap configuration for attach and launch - python

--- nvim/.config/nvim/lua/config/dap.lua
+++ nvim/.config/nvim/lua/config/dap.lua
@@ -22,30 +22,99 @@ function M.end_debug_session()
     require 'dapui'.close()
 end
 
+local enrich_config = function(config, on_config)
+    if not config.pythonPath and not config.python then
+        config.pythonPath = "/usr/bin/python3"
+    end
+    on_config(config)
+end
+
 function M.setup()
-    vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapStopped', { text = '🟢', texthl = '', linehl = '', numhl = '' })
 
     local dap = require('dap')
+
+    -- python: https://github.com/mendes-davi/nvim-config/blob/daf7e8f9974cd4e715c171071b8e3b460365fea9/lua/dap/python.lua
+    local opts = vim.tbl_extend("keep", opts or {}, {})
+    dap.adapters.python = function(cb, config)
+        if config.request == "attach" then
+            local port = (config.connect or config).port
+            cb {
+                type = "server",
+                port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+                host = (config.connect or config).host or "127.0.0.1",
+                enrich_config = enrich_config,
+                options = {
+                    source_filetype = "python",
+                },
+            }
+        else
+            cb {
+                type = "executable",
+                command = "/usr/bin/python3",
+                args = { "-m", "debugpy.adapter" },
+                enrich_config = enrich_config,
+                options = {
+                    source_filetype = "python",
+                },
+            }
+        end
+    end
+
+    dap.configurations.python = dap.configurations.python or {}
+    table.insert(dap.configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        console = opts.console,
+        pythonPath = opts.pythonPath,
+    })
+    table.insert(dap.configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "Launch file with arguments",
+        program = "${file}",
+        args = function()
+            local args_string = vim.fn.input "Arguments: "
+            return vim.split(args_string, " +")
+        end,
+        console = opts.console,
+        pythonPath = opts.pythonPath,
+    })
+    table.insert(dap.configurations.python, {
+        type = "python",
+        request = "attach",
+        name = "Attach remote",
+        connect = function()
+            local host = vim.fn.input "Host [127.0.0.1]: "
+            host = host ~= "" and host or "127.0.0.1"
+            local port = tonumber(vim.fn.input "Port [5678]: ") or 5678
+            return { host = host, port = port }
+        end,
+    })
+
     dap.adapters.cppdbg = {
         id = 'cppdbg',
         type = 'executable',
-        command = 'OpenDebugAD7',
+        command = os.getenv("HOME") .. "/.cpptools/cpptools-linux/extension/debugAdapters/bin/OpenDebugAD7",
     }
 
-    dap.adapters.python = {
-        type = 'executable';
-        command = 'python';
-        args = { '-m', 'debugpy.adapter' };
-    }
+    -- Dap load launch.json from vscode when avaliable
+    if vim.fn.filereadable("./.vscode/launch.json") or vim.fn.filereadable("./launch.json") then
+        require('dap.ext.vscode').load_launchjs(nil, { cppdbg = { 'c', 'cpp' } })
+    end
 
-    dap.configurations.python = {
+    dap.configurations.cpp = {
         {
-            type = 'python',
+            type = 'cppdbg',
             request = 'launch',
             name = "Launch file",
-            program = "${file}",
+            program = "/home/fra/dev/leetcode/bazel-bin/problems/test/unit_tests",
             args = {},
-            pythonPath = function() return '/usr/bin/python' end,
+            cwd = "/home/fra/dev/leetcode",
+            cppdbgPath = os.getenv("HOME") .. "/.cpptools/cpptools-linux/extension/debugAdapters/bin/OpenDebugAD7",
         },
     }
 
@@ -62,6 +131,13 @@ function M.setup()
             elements = { "repl" },
         },
     })
+end
+
+local load_launchjs = function()
+    require("dap.ext.vscode").load_launchjs()
+end
+if not pcall(load_launchjs) then
+    vim.notify("Failed to parse launch.json", "warn")
 end
 
 return M
