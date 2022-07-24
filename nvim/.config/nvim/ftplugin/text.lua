2a5388d48ecc3dae48de66e7e5004c3017e6cff1
Franek Borowiec <franek.borowiec@gmail.com>
2022-07-24 12:03:58 +0200

    Ftplugin for text

--- nvim/.config/nvim/ftplugin/text.lua
+++ nvim/.config/nvim/ftplugin/text.lua
@@ -1,4 +1,4 @@
 -- https://github.com/f3fora/cmp-spell
 -- Setting spell (and spelllang) is mandatory to use spellsuggest.
-vim.opt.spell = true
+vim.opt.spell = false
 vim.opt.spelllang = { 'en_us' }
