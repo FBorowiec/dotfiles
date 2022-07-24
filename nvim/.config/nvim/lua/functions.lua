6b6ca4bf16eb925b7b8cb6c7adb823b13bcf7472
Franek Borowiec <franek.borowiec@gmail.com>
2022-07-24 13:04:53 +0200

    Mappings, plugins and functions

--- nvim/.config/nvim/lua/functions.lua
+++ nvim/.config/nvim/lua/functions.lua
@@ -19,3 +19,14 @@ vim.cmd([[
         exec "find " . filepath . filename . filetype
     endfun
 ]])
+--
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
