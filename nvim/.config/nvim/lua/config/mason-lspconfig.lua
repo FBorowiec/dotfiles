52bea7e545cb705b3ba6e9a017a8d11ff3369271
Franek Borowiec <franek.borowiec@gmail.com>
2022-10-23 10:33:54 +0200

    Update mason config

--- nvim/.config/nvim/lua/config/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/config/mason-lspconfig.lua
@@ -56,7 +56,7 @@ function M.setup()
     require("mason-lspconfig").setup({ ensure_installed = servers, automatic_installation = true })
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
     capabilities.offsetEncoding = { "utf-16" }
 
     for _, server in pairs(servers) do
