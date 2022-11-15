fefae76fe465dfc71f73d816e1f5eaa9d126d303
Franek Borowiec <franek.borowiec@gmail.com>
2022-11-15 23:14:40 +0100

    Added markdown setup

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,4 +1,10 @@
 -- https://github.com/f3fora/cmp-spell
 -- Setting spell (and spelllang) is mandatory to use spellsuggest.
-vim.opt.spell = true
-vim.opt.spelllang = { 'en_us' }
+vim.opt_local.spell = true
+vim.opt_local.spelllang = { 'en_us' }
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.textwidth = 80
+vim.g.markdown_folding = 0
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -158,7 +158,7 @@ map('n', '<leader>gj', ':diffget //3<cr>', options)
 map('n', '<leader>gf', ':diffget //2<cr>', options)
 map('n', '<leader>gs', ':G<cr>', options) -- gv on a file (inside the status menu) to resolve conflicts
 map('n', '<leader>gb', ':Git blame<cr>', options)
-map('n', '<leader>gd', ':Gdiff<cr>', options)
+map('n', '<leader>gd', ':Gdiffsplit<cr>', options)
 
 -- GITSIGNS -----------------------------------------------
 -- see config/gitsigns.lua
