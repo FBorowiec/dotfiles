a729e378306511e89ed15a6639f27c9a02ecbe8f
Franek Borowiec <franek.borowiec@gmail.com>
2023-05-19 13:42:48 +0200

    New mapping for git status

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -170,7 +170,8 @@ map('n', '<leader>m', ':MaximizerToggle!<cr>', options)
 -- GIT FUGITIVE -------------------------------------------
 map('n', '<leader>gj', ':diffget //3<cr>', options)
 map('n', '<leader>gf', ':diffget //2<cr>', options)
-map('n', '<leader>gs', ':G<cr>', options) -- gv on a file (inside the status menu) to resolve conflicts
+-- map('n', '<leader>gs', ':G<cr>', options) -- gv on a file (inside the status menu) to resolve conflicts
+map('n', '<c-s>', ':G<cr>', options) -- gv on a file (inside the status menu) to resolve conflicts
 map('n', '<leader>gb', ':Git blame<cr>', options)
 map('n', '<leader>gd', ':Gdiffsplit<cr>', options)
 
