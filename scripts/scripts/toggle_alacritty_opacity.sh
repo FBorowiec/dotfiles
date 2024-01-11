883ee0efe2cad5f3003ed4237f9b7fed8b8070e0
Franek Borowiec <franek.borowiec@gmail.com>
2024-01-11 13:52:35 +0100

    Updated Alacritty config to toml

--- scripts/scripts/toggle_alacritty_opacity.sh
+++ scripts/scripts/toggle_alacritty_opacity.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-CONFIG_FILE="$HOME/.config/alacritty/alacritty.yml"
+CONFIG_FILE="$HOME/.config/alacritty/alacritty.toml"
 OPACITY_VALUE="0.8"
 
 # Check if the config file exists
 if [ -f "$CONFIG_FILE" ]; then
 	# Check if the opacity line is present
-	if grep -q "opacity:" "$CONFIG_FILE"; then
+	if grep -q "opacity =" "$CONFIG_FILE"; then
 		# Toggle opacity value
-		if grep -q "opacity: $OPACITY_VALUE" "$CONFIG_FILE"; then
-			sed -i "s/opacity: $OPACITY_VALUE/opacity: 1.0/" "$CONFIG_FILE"
+		if grep -q "opacity = $OPACITY_VALUE" "$CONFIG_FILE"; then
+			sed -i "s/opacity = $OPACITY_VALUE/opacity = 1.0/" "$CONFIG_FILE"
 		else
-			sed -i "s/opacity: [0-9.]*$/opacity: $OPACITY_VALUE/" "$CONFIG_FILE"
+			sed -i "s/opacity = [0-9.]*$/opacity = $OPACITY_VALUE/" "$CONFIG_FILE"
 		fi
-	else
-		# If the opacity line doesn't exist, add it
-		echo "background_opacity: $OPACITY_VALUE" >>"$CONFIG_FILE"
+	# else
+	# 	# If the opacity line doesn't exist, add it
+	# 	echo "opacity = $OPACITY_VALUE" >>"$CONFIG_FILE"
 	fi
 
 else
