725ba78128f1fe83e07be1ffefbdb26c6f691506
Franek Borowiec <franciszek.borowiec@stellantis.com>
2024-09-25 01:28:39 +0200

    Added live grep with args mapping

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,10 +3,16 @@ return {
   dependencies = {
     {
       "nvim-telescope/telescope-live-grep-args.nvim",
-      version = "^1.0.0",
     },
   },
-  opts = function(_, opts)
+  keys = {
+    {
+      "<leader>sg",
+      "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      desc = "Grep (root dir)",
+    },
+  },
+  config = function(_, opts)
     opts.defaults = opts.defaults or {}
     opts.pickers = opts.pickers or {}
 
@@ -16,7 +22,7 @@ return {
     }
 
     local telescope = require("telescope")
+    telescope.setup(opts)
     telescope.load_extension("live_grep_args")
-    return opts
   end,
 }
