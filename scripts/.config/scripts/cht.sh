ff121568b201d49dd3fca3161a37167c1f437efa
Franek Borowiec <franciszek.borowiec@stellantis.com>
2024-09-26 23:33:25 +0200

    Moved scripts

--- scripts/.config/scripts/cht.sh
+++ scripts/.config/scripts/cht.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.config/.tmux-cht-languages ~/.config/.tmux-cht-command | fzf)
+selected=$(cat ~/.config/scripts/.tmux-cht-languages ~/.config/scripts/.tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
 	exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.config/scripts/.tmux-cht-languages; then
 	query=$(echo "$query" | tr ' ' '+')
 	tmux neww bash -c "curl cht.sh/$selected/$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 else
