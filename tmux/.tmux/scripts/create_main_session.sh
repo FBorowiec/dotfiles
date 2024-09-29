6984899c6d6a679a80ea087b64034d773b9099fa
Franek Borowiec <franek.borowiec@gmail.com>
2024-09-29 16:02:24 +0200

    Updated session scripts

--- tmux/.tmux/scripts/create_main_session.sh
+++ tmux/.tmux/scripts/create_main_session.sh
@@ -2,11 +2,19 @@
 
 SESSION_NAME="Main"
 
+display_message() {
+	if [ "$TMUX" != "" ]; then
+		tmux display-message -d 1000 "$1"
+	else
+		echo "$1"
+	fi
+}
+
 # Check if the "Main" session already exists
 if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-	echo "Session '$SESSION_NAME' already exists."
+	display_message "Session '$SESSION_NAME' already exists. Attaching to it."
 else
-	echo "Creating session '$SESSION_NAME'."
+	display_message "Creating session '$SESSION_NAME'."
 	# Create the "Main" session and set up windows
 	tmux new-session -d -s "$SESSION_NAME" -n "Ranger" -c "$PWD" "ranger"
 	tmux new-window -t "$SESSION_NAME:2" -n "Dotfiles" -c "$HOME/dotfiles"
@@ -20,13 +28,13 @@ fi
 if [ "$TMUX" != "" ]; then
 	CURRENT_SESSION=$(tmux display-message -p '#S')
 	if [ "$CURRENT_SESSION" != "$SESSION_NAME" ]; then
-		echo "Switching from session '$CURRENT_SESSION' to '$SESSION_NAME'."
+		display_message "Switching from session '$CURRENT_SESSION' to '$SESSION_NAME'."
 		tmux switch-client -t "$SESSION_NAME"
 		tmux kill-session -t "$CURRENT_SESSION"
 	else
-		echo "Already in session '$SESSION_NAME'."
+		display_message "Already in session '$SESSION_NAME'."
 	fi
 else
-	echo "Attaching to session '$SESSION_NAME'."
+	display_message "Attaching to session '$SESSION_NAME'."
 	tmux attach-session -t "$SESSION_NAME"
 fi
