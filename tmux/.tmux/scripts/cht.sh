35a92679fe454ab7ebc8df92ad52e5a6b8021aa8
Franek Borowiec <franek.borowiec@gmail.com>
2024-09-29 14:34:10 +0200

    Added scripts

--- tmux/.tmux/scripts/cht.sh
+++ tmux/.tmux/scripts/cht.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.config/scripts/.tmux-cht-languages ~/.config/scripts/.tmux-cht-command | fzf)
+scripts_path="$HOME/.tmux/scripts"
+languages="$scripts_path"/.tmux-cht-languages
+commands="$scripts_path"/.tmux-cht-command
+
+selected=$(cat "$languages" "$commands" | fzf)
 if [[ -z $selected ]]; then
 	exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/scripts/.tmux-cht-languages; then
+if grep -qs "$selected" "$languages"; then
 	query=$(echo "$query" | tr ' ' '+')
 	tmux neww bash -c "curl cht.sh/$selected/$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 else
