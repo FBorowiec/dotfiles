1a39a268d6c26c7cde4e5b0ad4474a07b3a8c8ec
Franek Borowiec <franek.borowiec@gmail.com>
2024-10-07 17:16:43 +0200

    Updated tmux scripts

--- tmux/.tmux/scripts/create_main_session.sh
+++ tmux/.tmux/scripts/create_main_session.sh
@@ -10,17 +10,14 @@ display_message() {
 	fi
 }
 
-# Check if the "Main" session already exists
 if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 	display_message "Session '$SESSION_NAME' already exists. Attaching to it."
 else
 	display_message "Creating session '$SESSION_NAME'."
-	# Create the "Main" session and set up windows
 	tmux new-session -d -s "$SESSION_NAME" -n "Ranger" -c "$PWD" "ranger"
 	tmux new-window -t "$SESSION_NAME:2" -n "Dotfiles" -c "$HOME/dotfiles"
 	tmux new-window -t "$SESSION_NAME:3" -n "Ansible" -c "$HOME/ansible"
-	tmux new-window -t "$SESSION_NAME:4" -n "Dev 1" -c "$HOME"
-	tmux new-window -t "$SESSION_NAME:5" -n "Dev 2" -c "$HOME"
+	tmux new-window -t "$SESSION_NAME:4" -n "Term" -c "$HOME"
 	tmux new-window -t "$SESSION_NAME:9" -n "BTOP" -c "$PWD" "btop"
 	tmux select-window -t "$SESSION_NAME:1"
 fi
