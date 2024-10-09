9ff38aef66ff3bbcfe3b850ee24e5a29108af26b
Franek Borowiec <franciszek.borowiec@stellantis.com>
2024-10-09 14:56:13 +0200

    Renamed windows in tmux

--- tmux/.tmux/scripts/create_dev_session.sh
+++ tmux/.tmux/scripts/create_dev_session.sh
@@ -15,7 +15,7 @@ else
 	# No session name provided, generate "Term N"
 	session_number=1
 	while true; do
-		SESSION_NAME="Term $session_number"
+		SESSION_NAME="Dev $session_number"
 		if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 			session_number=$((session_number + 1))
 		else
@@ -28,9 +28,9 @@ if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 	display_message "Session '$SESSION_NAME' already exists. Attaching to it."
 else
 	display_message "Creating session '$SESSION_NAME'."
-	tmux new-session -d -s "$SESSION_NAME" -n "Ranger" -c "$PWD" "ranger"
-	tmux new-window -t "$SESSION_NAME:2" -n "Term" -c "$PWD"
-	tmux new-window -t "$SESSION_NAME:9" -n "BTOP" -c "$PWD" "btop"
+	tmux new-session -d -s "$SESSION_NAME" -n "ranger" -c "$PWD" "ranger"
+	tmux new-window -t "$SESSION_NAME:2" -n "zsh" -c "$PWD"
+	tmux new-window -t "$SESSION_NAME:9" -n "btop" -c "$PWD" "btop"
 	tmux select-window -t "$SESSION_NAME:1"
 fi
 
