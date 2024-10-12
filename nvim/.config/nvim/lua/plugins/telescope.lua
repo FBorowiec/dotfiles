b95cf8ea0d81064f7338b61c71266c01c05e20f4
Franek Borowiec <franek.borowiec@gmail.com>
2024-10-12 23:21:19 +0200

    Added additional mappings for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,7 +12,17 @@ return {
     {
       "<leader>sg",
       "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-      desc = "Grep with args (root dir)",
+      desc = " Grep with args (root dir)",
+    },
+    {
+      "<leader>ff",
+      "<cmd>Telescope git_files<CR>",
+      desc = "Find Files (git-files)",
+    },
+    {
+      "<leader>fg",
+      "<cmd>Telescope find_files<CR>",
+      desc = "Find Files (Root Dir)",
     },
   },
   config = function()
@@ -23,6 +33,25 @@ return {
             ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
             ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " }),
             ["<C-space>"] = actions.to_fuzzy_refine,
+          },
+        },
+      },
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob",
+            "!.git/",
+            "--ignore-file",
+            ".gitignore",
           },
         },
       },
