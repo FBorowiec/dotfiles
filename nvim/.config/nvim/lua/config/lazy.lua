2a8672e67c151d63a8a90b8e319523f55604e633
Franek Borowiec <franek.borowiec@gmail.com>
2024-10-12 23:45:42 +0200

    Disable nix

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -70,7 +70,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.lean" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.nix" },
+    -- { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.nushell" },
     { import = "lazyvim.plugins.extras.lang.ocaml" },
     -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
