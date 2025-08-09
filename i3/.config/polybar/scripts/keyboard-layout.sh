eebbb356ca67cffe35e1df8498d58a39ad2f2654
Franek Borowiec <franek.borowiec@gmail.com>
2025-08-09 20:37:34 +0200

    keyboard layout script

---
+++ i3/.config/polybar/scripts/keyboard-layout.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cycle through keyboard layouts: US -> Polish -> French -> Italian -> US
+current=$(setxkbmap -query | grep layout | awk '{print $2}')
+
+case $current in
+"us")
+	setxkbmap pl
+	notify-send "Keyboard Layout" "Switched to Polish (PL)" -t 1500
+	;;
+"pl")
+	setxkbmap fr
+	notify-send "Keyboard Layout" "Switched to French (FR)" -t 1500
+	;;
+"fr")
+	setxkbmap it
+	notify-send "Keyboard Layout" "Switched to Italian (IT)" -t 1500
+	;;
+"it")
+	setxkbmap us
+	notify-send "Keyboard Layout" "Switched to US English (US)" -t 1500
+	;;
+*)
+	# Default to US if unknown layout
+	setxkbmap us
+	notify-send "Keyboard Layout" "Switched to US English (US)" -t 1500
+	;;
+esac
