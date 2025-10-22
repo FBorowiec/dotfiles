aa54113af16ce12e7497df8c98b0ea71f5e658e0
Franek Borowiec <franek.borowiec@gmail.com>
2025-10-22 11:03:41 +0200

    disable format on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,8 @@ return {
     event = { "BufWritePre" },
     opts = {
       formatters_by_ft = {
-        python = { "ruff_format" },  -- Use ruff format from pyproject.toml
+        python = { "ruff_format" }, -- Use ruff format from pyproject.toml
         markdown = { "prettier", "markdownlint-cli2" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
       },
       formatters = {
         ["markdownlint-cli2"] = {
